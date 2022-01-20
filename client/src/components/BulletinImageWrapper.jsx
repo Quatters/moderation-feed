736163ac087cce170acccdb37ccf00f007281e75
Quatters <vladnfs3@gmail.com>
2022-01-20 19:43:32 +1000

    images downloads from server

--- client/src/components/BulletinImageWrapper.jsx
+++ client/src/components/BulletinImageWrapper.jsx
@@ -4,14 +4,11 @@ import '../style/BulletinImageWrapper.css';
 function BulletinImageWrapper(props) {
   return (
     <div className='bulletin-image-wrapper'>
-      <img
-        src='https://static.baza.farpost.ru/v/1510541224458_hugeBlock'
-        alt=''
-      />
-      {/* <img
-        src='https://static.baza.farpost.ru/v/1510541224458_hugeBlock'
-        alt=''
-      /> */}
+      {props.images.map((image, i) => (
+        <div key={i}>
+          <img src={image} alt='' />
+        </div>
+      ))}
     </div>
   );
 }
